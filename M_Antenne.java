3f9ff5a9d5b135d55382c421da8387ba9421e2f1
alexixi83 <51537609+alexixi83@users.noreply.github.com>
2020-03-03 10:19:39 +0100

    Add files via upload

--- M_Antenne.java
+++ M_Antenne.java
@@ -11,8 +11,7 @@ public class M_Antenne {
 	//Constantes
 	public static final double FREQUENCE = 0;
 	public static final int PUISSANCE_EMISSION = 0;
-	public static final int SENSIBILITE = 0;
-	public static final String POLARISATION = "inconnu";
+
 	
 	//variable de la classe
 	public static int cmpMAntenne = 0;
